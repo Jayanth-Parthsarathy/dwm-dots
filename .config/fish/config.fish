7a64b93a16947f88493da9cc3c82301c2147aa13
jayanth-parthsarathy <jparthaa@gmail.com>
2023-06-08 14:59:57 +0530

    Add fzf for fuzzy-finding

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
     alias config '/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME' 
+
+    # Key bindings for fzf
+    function fish_user_key_bindings
+        # Reverse history search with Ctrl + R
+        bind \cr 'fzf-history'
+
+        # File search with Ctrl + T
+        bind \ct 'fzf-files'
+    end
 end
+
 starship init fish | source
+
